0958e46b94f14a01b0635a8cd5b717871ba0add9
Anikt Dixit <ankitdixit29269@gmail.com>
2023-01-28 14:07:14 +0530

    code in js question 1

---
+++ javascript/01 Question/variables.js
@@ -0,0 +1,18 @@
+// string
+let name = 'Ankit';
+console.log(typeof name);
+
+// boolean
+let abc = 15;
+let cs = 20;
+let all = abc>cs;
+console.log(all);
+console.log(typeof all);
+
+// undefined
+let x;
+console.log(typeof x);
+
+// null
+let s = null;
+console.log(typeof s);
