f78c1878bd7f99946c45cc97df556c56fc1009b6
Anikt Dixit <ankitdixit29269@gmail.com>
2023-01-31 17:32:22 +0530

    adding codes for question  15,14

---
+++ javascript/30 Question in javascript/15 Question/index.js
@@ -0,0 +1,14 @@
+let x = prompt('give no ');
+if (x>100){
+    document.getElementById('p').innerHTML = "out range"
+} else if(x>=80 && x<=100){
+    document.getElementById('p').innerHTML = "A grade"
+}else if(x>=70) {
+    document.getElementById('p').innerHTML = "B grade"
+}else if(x>=60){
+    document.getElementById('p').innerHTML = "C grade"
+}else if(x>=50){
+    document.getElementById('p').innerHTML = "D grade"
+}else{
+    document.getElementById('p').innerHTML = "fail"
+}
