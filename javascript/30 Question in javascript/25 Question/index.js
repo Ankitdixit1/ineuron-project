551ed74f342c47c9e46513e862745d36137f4f85
Anikt Dixit <ankitdixit29269@gmail.com>
2023-02-05 22:08:55 +0530

    adding code for question 25

--- javascript/30 Question in javascript/25 Question/index.js
+++ javascript/30 Question in javascript/25 Question/index.js
@@ -6,4 +6,8 @@ if (bmi<18.5){
     console.log('Underweight');
 }else if (bmi=>18.5 && bmi<24.9){
     console.log('Normal weight');
+}else if (bmi=>25 && bmi<29.9){
+    console.log('overweight');
+}else if (bmi=>30){
+    console.log('obese');
 }
