801bc2bd4194f633a0a40e094eaf16f616927e51
Anikt Dixit <ankitdixit29269@gmail.com>
2023-02-05 11:54:50 +0530

    adding code

--- javascript/30 Question in javascript/23 Question/index.js
+++ javascript/30 Question in javascript/23 Question/index.js
@@ -1,1 +1,25 @@
-let x = prompt('no that you want to know ');
+
+let number = prompt('enter a positive no');
+let isPrime = true;
+if (number === 1) {
+    console.log("1 is neither prime nor composite number.");
+}
+
+// check if number is greater than 1
+else if (number > 1) {
+
+    // looping through 2 to number-1
+    for (let i = 2; i < number; i++) {
+        if (number % i == 0) {
+            console.log(i);
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime) {
+        console.log(`${number} is a prime number`);
+    } else {
+        console.log(`${number} is a not prime number`);
+    }
+}
