3808fd6a92c88388c409fa6e50cd8a9c5295f88c
Anikt Dixit <ankitdixit29269@gmail.com>
2023-02-05 22:07:07 +0530

    adding code

---
+++ javascript/30 Question in javascript/24 Question/index.js
@@ -0,0 +1,9 @@
+let fd = [];
+let sd = [];
+for (let i = 0; i<=100;i++){
+    if (i%2==0){
+        fd.push(i)
+    }else{
+        sd.push(i)
+    }
+}
---
+++ javascript/30 Question in javascript/25 Question/index.js
@@ -0,0 +1,13 @@
+let x = prompt('your weight in kg');
+let y = prompt('your height in cm')
+let bmi = Math.round(x/((y/100)*(y/100)));
+console.log(bmi);
+if (bmi<18.5){
+    console.log('Underweight');
+}else if (bmi=>18.5 && bmi<24.9){
+    console.log('Normal weight');
+}else if (bmi=>25 && bmi<29.9){
+    console.log('overweight');
+}else if (bmi=>30){
+    console.log('obese');
+}
