e36b99c41ddc0e8cbacf21af3dc65fc4eb6bb568
Anikt Dixit <ankitdixit29269@gmail.com>
2023-02-06 20:08:55 +0530

    code for question 28

--- javascript/30 Question in javascript/28 Question/index.js
+++ javascript/30 Question in javascript/28 Question/index.js
@@ -1,13 +1,28 @@
-// A.  Print a triangle pattern,
+// // A.  Print a triangle pattern,
 // let a =3;
 // for(let i=1;i<=a;i++){
 //     console.log("*".repeat(i));
 // }
 
-// b. Print a square pattern,
+// // b. Print a square pattern,
 // let b = 3
 // for (let i=1;i<=b;i++){
 //     console.log("*".repeat(b));
 // }
 
-
+// // c. Print a pyramid pattern,
+// let n = 3;
+// let string = "";
+// // External loop
+// for (let i = 1; i <= n; i++) {
+//     // printing spaces
+//     for (let j = 1; j <= n - i; j++) {
+//         string += " ";
+//     }
+//     // printing star
+//     for (let k = 0; k < 2 * i - 1; k++) {
+//         string += "*";
+//     }
+//     string += "\n";
+// }
+// console.log(string);
