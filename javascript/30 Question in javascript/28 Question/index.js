ab797308a795a7997ff6d6d66067f186c8e799b9
Anikt Dixit <ankitdixit29269@gmail.com>
2023-02-06 17:19:51 +0530

    code for question 28

---
+++ javascript/30 Question in javascript/28 Question/index.js
@@ -0,0 +1,13 @@
+// A.  Print a triangle pattern,
+// let a =3;
+// for(let i=1;i<=a;i++){
+//     console.log("*".repeat(i));
+// }
+
+// b. Print a square pattern,
+// let b = 3
+// for (let i=1;i<=b;i++){
+//     console.log("*".repeat(b));
+// }
+
+
