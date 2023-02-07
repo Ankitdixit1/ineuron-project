5f86d2b423c32d1e1e6669d0d41fe77653bad48b
Anikt Dixit <ankitdixit29269@gmail.com>
2023-02-07 17:24:16 +0530

    code for question 30

---
+++ javascript/30 Question in javascript/30 Question/index.js
@@ -0,0 +1,5 @@
+let x = prompt("what you want to calculate ?");
+let y = eval(x);
+console.log(y);
+// this is the simplest way by which user can calculte there are more certain way for
+//  calculate but by taking input the way can be prompt. other can be like by using condition
