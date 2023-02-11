de14341d49e6ed349683fe7c5ca231fa81828cc0
Anikt Dixit <ankitdixit29269@gmail.com>
2023-02-11 17:31:13 +0530

    code

---
+++ DOM Assignments 2.0 Batch copy/DOM P8/DOM P8/index.js
@@ -0,0 +1,11 @@
+// let create = document.createElement("h2");
+// create.className = "new-head";
+// create.innerHTML = "This is custom heading";
+// let createp =  document.createElement("p");
+// createp.className = "new-p";
+// createp.innerHTML = "here i dont what should i right so i am writing some line ";
+// let select = document.querySelector(".new");
+// select.style.overflowY = "scroll"
+// select.appendChild(create)
+// select.appendChild(createp)
+// select.style.border = "3px solid red"
