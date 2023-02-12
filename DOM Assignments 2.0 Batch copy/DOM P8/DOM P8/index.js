736fd49f7de2694e8585dd7f80ea3cdca621d51e
Anikt Dixit <ankitdixit29269@gmail.com>
2023-02-12 15:01:58 +0530

    adding code for project 8

--- DOM Assignments 2.0 Batch copy/DOM P8/DOM P8/index.js
+++ DOM Assignments 2.0 Batch copy/DOM P8/DOM P8/index.js
@@ -1,3 +1,5 @@
+// Task 1
+
 // let create = document.createElement("h2");
 // create.className = "new-head";
 // create.innerHTML = "This is custom heading";
@@ -9,3 +11,30 @@
 // select.appendChild(create)
 // select.appendChild(createp)
 // select.style.border = "3px solid red"
+
+// task 2
+// document.body.style.background = "white"
+
+// let accordian = document.querySelector(".navbar-toggler");
+// accordian.onclick = solid();
+// console.log(accordian);
+
+// function solid(){
+//     let para = document.querySelector(".collapse");
+//     console.log(para);
+//     if (para.style.display === "block") {
+//       para.style.display = "none";
+//     } else {
+//       para.style.display = "block";
+//     }
+// }
+// let accordian = document.querySelector(".navbar-toggler");
+//   accordian.addEventListener("click", () => {
+//     let select = document.querySelector(".collapse");
+//     if (select.style.display === "block") {
+//       select.style.display = "none";
+//     } else {
+//       select.style.display = "block";
+//     }
+//   });
+
