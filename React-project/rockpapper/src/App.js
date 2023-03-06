61bf3f5f26bdb3d129647ee2c8893d8cd683ca79
Anikt Dixit <ankitdixit29269@gmail.com>
2023-03-06 22:13:31 +0530

    react code

---
+++ React-project/rockpapper/src/App.js
@@ -0,0 +1,20 @@
+import Card from "./card";
+import Axios from 'axios';
+import { useState } from "react";
+
+// const [details, setdetails] = useState({})
+
+// const featchdetails = async () =>{
+
+// }
+
+function App() {
+  return (
+    <>
+    <h1>hellos </h1>
+    <Card name = "ankit"/>
+    </>
+  );
+}
+
+export default App;
---
+++ React-project/rockpapper/src/card.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function card({name}){
+    return(
+        <h1>my name is {name}</h1>
+    )
+}
+export default card
