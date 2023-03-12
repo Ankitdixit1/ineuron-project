03913b097b55bfaea772da4bed9beec69b8fc0f6
Anikt Dixit <ankitdixit29269@gmail.com>
2023-03-12 10:39:18 +0530

    code

--- React-project/todolist/src/App.js
+++ React-project/todolist/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from './logo.png'
+import  './styles.css'
+import {useState} from 'react';
+import { useEffect } from 'react';
 function App() {
+  const [show, setshow] =useState("none")
+  const dis = ()=>{
+    setshow("block")
+    console.log(show);
+  }
+
+  
+    // const hideOnPageClick = () => {
+    //   console.log(show);
+    //   if(show == "block"){
+    //     setshow("none");
+    //     // console.log("so");
+    //   }
+    // };
+    // document.addEventListener("click", hideOnPageClick);
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <img src={logo}></img>
+    <span>Keep Note</span>
+    <form>
+      <input type="text" placeholder="Title" onClick={dis}></input>
+      <p style={{display: show}}>
+        <textarea name="content" placeholder="make note.."></textarea>
+      </p>
+    </form>
+    </>
   );
 }
 
