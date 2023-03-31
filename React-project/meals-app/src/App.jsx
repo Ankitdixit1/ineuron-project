99c1252c06d17419dd7f6417bd5568ba1db1ae55
Anikt Dixit <ankitdixit29269@gmail.com>
2023-03-31 20:51:21 +0530

    adding codes

--- React-project/meals-app/src/App.jsx
+++ React-project/meals-app/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [meal, setMeal] = useState(null);
+  const [searchValue, setSearchValue] = useState("");
 
+  const handleSearch = async () => {
+    const { data } = await Axios.get(
+      `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`
+    );
+    setMeal(data.meals?.[0]);
+  };
+  
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="text-center">
+      <div className={`${meal ? 'my-5' : 'my-[244px]'}`}>
+      <input
+        type="text"
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+        placeholder="ex-burger"
+      />
+      <button onClick={handleSearch} className="bg-orange-200 ml-[5px]">Search</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {meal && (
+        <div className="">
+          <h1>{meal.strMeal}</h1>
+          <div className="lg:flex lg:justify-center mx-auto lg:items-center text-lg lg:text-xl">
+            <img
+              src={meal.strMealThumb}
+              alt="Meal thumbnail"
+              className="w-60 mx-auto my-[13px] lg:h-[450px] lg:w-[450px]"
+            />
+            <h2 className="bg-orange-200 lg:invisible">Ingredients:</h2>
+            <ul className=" text-start mx-auto inline-block list-decimal">
+              {Array.from({ length: 20 }).map((_, index) => {
+                const ingredient = meal[`strIngredient${index + 1}`];
+                const measure = meal[`strMeasure${index + 1}`];
+                if (!ingredient) return null;
+                return (
+                  <li key={ingredient}>
+                    {measure} {ingredient}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
