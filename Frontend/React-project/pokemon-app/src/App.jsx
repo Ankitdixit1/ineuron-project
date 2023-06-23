ac375f63e36825e79563c5e94760cdb69d00ea66
ankitdixit <ankitdixit29269@gmail.com>
2023-06-23 23:50:36 +0530

    adding some code to pokemon-app

--- Frontend/React-project/pokemon-app/src/App.jsx
+++ Frontend/React-project/pokemon-app/src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import React, { useState } from 'react';
 import './App.css'
+import Axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+const [getpokemon,setpokemon] = useState('')
+const[search,setsearch]=useState('');
+const searchpok = async() => {
+  const {data} = await Axios.get(`https://pokeapi.co/api/v2/pokemon/${search}`);
+  
+}
   return (
     <>
-
+    <h1>hellow</h1>
+    <input type="text" value={search} onChange={(e)=> setsearch(e.target.value)} placeholder='ditto'/>
+    <button onClick={searchpok}>search</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
