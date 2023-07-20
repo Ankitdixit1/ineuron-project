2af0629f3a12d053918a5e6297263592eaaa7457
ankitdixit <ankitdixit29269@gmail.com>
2023-07-20 15:14:27 +0530

    completed the hole project

--- Frontend/React-project/Movie-app/src/pages/home/trending/Trending.jsx
+++ Frontend/React-project/Movie-app/src/pages/home/trending/Trending.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-// import Carousel from "../../../components/carousel/Carousel";
+import Carousel from "../../../components/carousel/Carousel";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 
@@ -9,10 +9,10 @@ import useFetch from "../../../hooks/useFetch";
 const Trending = () => {
     const [endpoint, setEndpoint] = useState("day");
 
-    // const { data, loading } = useFetch(`/trending/movie/${endpoint}`);
+    const { data, loading } = useFetch(`/trending/movie/${endpoint}`);
 
     const onTabChange = (tab) => {
-        // setEndpoint(tab === "Day" ? "day" : "week");
+        setEndpoint(tab === "Day" ? "day" : "week");
     };
 
     return (
@@ -21,7 +21,7 @@ const Trending = () => {
                 <span className="carouselTitle">Trending</span>
                 <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
             </ContentWrapper>
-            {/* <Carousel data={data?.results} loading={loading} /> */}
+            <Carousel data={data?.results} loading={loading} />
         </div>
     );
 };
